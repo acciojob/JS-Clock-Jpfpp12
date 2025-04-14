4710127c7fbedafcfbb7c03be971e4ee0179478c
Jpfpp12 <118419073+Jpfpp12@users.noreply.github.com>
2025-04-14 19:27:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+
+function updateClock() {
+  const now = new Date();
+
+  const second = now.getSeconds();
+  const minute = now.getMinutes();
+  const hour = now.getHours();
+
+  const secondDeg = second * 6;
+  const minuteDeg = minute * 6 + second * 0.1;
+  const hourDeg = (hour % 12) * 30 + minute * 0.5;
+  document.querySelector('.second-hand').style.transform = `rotate(${secondDeg}deg)`;
+  document.querySelector('.minute-hand').style.transform = `rotate(${minuteDeg}deg)`;
+  document.querySelector('.hour-hand').style.transform = `rotate(${hourDeg}deg)`;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
